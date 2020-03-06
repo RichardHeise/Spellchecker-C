c10de59f4fdd846e1001f8104969404ce443b503
RichardHeise <richardfhf5@gmail.com>
2020-03-06 00:25:05 -0300

    Empty files, setting env
    Signed-off-by: RichardHeise <richardfhf5@gmail.com>

---
+++ ortografia.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+    char *entrada;
+    int tamanho;
+
+    tamanho = 1000;
+    malloc (tamanho * sizeof (char));
+
+    readDict(entrada);
+
+    
+}

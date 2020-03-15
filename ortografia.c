eafba23998b6155c1e244609cd8e24b10c4d3806
RichardHeise <richardfhf5@gmail.com>
2020-03-15 03:48:52 -0300

    allocation function done
    Signed-off-by: RichardHeise <richardfhf5@gmail.com>

--- ortografia.c
+++ ortografia.c
@@ -2,14 +2,51 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
-    char *entrada;
-    int tamanho;
+void open_file (FILE **file_pointer) {
+    (*file_pointer) = fopen("brazilian", "r+");
 
-    tamanho = 1000;
-    malloc (tamanho * sizeof (char));
+    if (!(*file_pointer)) {
+        printf("Error: couldn't open file.");
+        exit(1);
+    }
+}
 
-    readDict(entrada);
+void matrix_allocantion (unsigned char ***matrix) {
+    *matrix = (unsigned char **)malloc(sizeof(unsigned char *));
+    if ((*matrix) == NULL) {
+        printf("Error: couldn't allocate memory.");
+        exit(2);
+    }
 
+    (*matrix)[0] = (unsigned char *)malloc(sizeof(unsigned char));
+    if ((*matrix) == NULL) {
+        printf("Error: couldn't allocate memory.");
+        exit(2);
+    }
+}
+
+void matrix_reallocation (unsigned char ***matrix) {
+    *matrix = realloc(*matrix,  50 * sizeof(unsigned char *));
+    if ((*matrix) == NULL) {
+        printf("Error: couldn't allocate memory.");
+        exit(2);
+    }
+}
+
+void load_dict (FILE *file_pointer, char unsigned **dict_pointer) {
     
+}
+
+int main () {
+    
+    FILE *arq;
+    unsigned char **dict;
+
+    matrix_allocantion(&dict);
+    open_file(&arq);
+    
+
+
+    fclose(arq);
+    return 1;
 }

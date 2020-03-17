26adbd1485c4f8ec73903d9da9ef2c4b847cb996
RichardHeise <richardfhf5@gmail.com>
2020-03-16 22:42:02 -0300

    It works, almost...
    Signed-off-by: RichardHeise <richardfhf5@gmail.com>

--- ortografia.c
+++ ortografia.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
 
 typedef struct s_dict {
     unsigned char **dict;
@@ -8,7 +9,7 @@ typedef struct s_dict {
 } dictionary;
 
 void open_file (FILE **file_pointer) {
-    (*file_pointer) = fopen("brazilian", "r+");
+    (*file_pointer) = fopen("/usr/share/dict/brazilian", "r");
 
     if (!(*file_pointer)) {
         printf("Error: couldn't open file.");
@@ -17,13 +18,13 @@ void open_file (FILE **file_pointer) {
 }
 
 void dict_allocation (dictionary *dict_pointer) {
-    dict_pointer->dict = (unsigned char **)malloc(100 * sizeof(unsigned char *));
+    dict_pointer->dict = (unsigned char **)malloc(10000 * sizeof(unsigned char *));
     if (dict_pointer->dict == NULL) {
         printf("Error: couldn't allocate memory.");
         exit(2);
     }
     int i;
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < 10000; i++) {
         dict_pointer->dict[i] = (unsigned char *)malloc(sizeof(unsigned char));
         if (dict_pointer->dict == NULL) {
             printf("Error: couldn't allocate memory.");
@@ -33,13 +34,13 @@ void dict_allocation (dictionary *dict_pointer) {
 }
 
 void dict_reallocation (dictionary *dict_pointer) {
-    dict_pointer->dict = realloc(dict_pointer->dict,  (100 + dict_pointer->lines) * sizeof(unsigned char *));
+    dict_pointer->dict = realloc(dict_pointer->dict,  (10000 + dict_pointer->lines) * sizeof(unsigned char *));
     if (dict_pointer->dict == NULL) {
         printf("Error: couldn't allocate memory.");
         exit(2);
     }
     int i;
-    for (i = dict_pointer->lines; i < (100 + dict_pointer->lines); i++) {
+    for (i = dict_pointer->lines; i < (10000 + dict_pointer->lines); i++) {
         dict_pointer->dict[i] = (unsigned char *)malloc(sizeof(unsigned char));
         if (dict_pointer->dict == NULL) {
             printf("Error: couldn't allocate memory.");
@@ -48,23 +49,19 @@ void dict_reallocation (dictionary *dict_pointer) {
     }
 }
 
-int cmpfunc (const void * a, const void * b) {
-   return( strcmp( *((const char **)a), *((const char **)b)));
-}
-
-void load_dict (FILE **file_pointer,dictionary *dict_pointer) {
+void load_dict (FILE **file_pointer, dictionary *dict_pointer) {
     int i;
     for (i = 0; ! feof((*file_pointer)); i++ ) {
         fscanf((*file_pointer), "%s", dict_pointer->dict[i]);
         
-        if ( dict_pointer->lines % 98 == 0) {
+        dict_pointer->lines++;
+    
+        if ( dict_pointer->lines % 9999 == 0) {
             dict_reallocation(dict_pointer);
         }
-
-        dict_pointer->lines++;
+    
     }
 
-    qsort(dict_pointer->dict, dict_pointer->lines, sizeof(unsigned char *), cmpfunc);
     fclose((*file_pointer));
 }
 
@@ -73,16 +70,19 @@ void start_dict (dictionary *dict_pointer) {
     dict_allocation(dict_pointer);
 }
 
-void lower_case (unsigned char *value) {
-    if ( ((*value) >= 'A' && (*value) <= 'Z') ||
-        ((*value) >= 'À' && (*value) <= 'Ü') ) {
+unsigned char lower_case (unsigned char value) {
+    if ( (value >= 'A' && value <= 'Z') ||
+        (value >= 'À' && value <= 'Ü') ) {
 
-        (*value) += 32;
+        value += 32;
     }
+
+    return value;
+
 }
 
 int is_character (unsigned char value) {
-    lower_case(&value);
+    value = lower_case(value);
     if ((value >= 'a' && value <= 'z') ||
         (value == 'ç') ||
         (value >= 192 && value <= 252)) {
@@ -93,15 +93,21 @@ int is_character (unsigned char value) {
     return 0;
 }
 
-int dict_binary_search(dictionary *dict_pointer, unsigned char *str) {
+int dict_binary_search(dictionary *dict_pointer, unsigned char *str, int tam) {
     int start = 0;
     int end = dict_pointer->lines-1;
 
+    int i;
+    unsigned char tmp[50];
+    for (i = 0; i <= tam; i++) {
+        tmp[i] = lower_case(str[i]);
+    }
+
     while( start <= end ) {
         int mid = (start + end)/2;
-        if ( strcmp((char*)str, (char*)dict_pointer->dict[mid]) == 0) {
+        if ( strcmp((char*)tmp, (char*)dict_pointer->dict[mid]) == 0) {
             return mid;
-        } else if( strcmp((char*)str, (char*)dict_pointer->dict[mid]) < 0) {
+        } else if( strcmp((char*)tmp, (char*)dict_pointer->dict[mid]) < 0) {
             end = mid-1;
         } else {
             start = mid+1;
@@ -111,6 +117,7 @@ int dict_binary_search(dictionary *dict_pointer, unsigned char *str) {
 }
 
 int main () {
+    setlocale(LC_CTYPE, "pt_BR.iso88591");
     
     FILE *arq;
     dictionary pt_dict;
@@ -119,7 +126,7 @@ int main () {
     open_file(&arq);
     load_dict(&arq, &pt_dict);
 
-
+    
     unsigned char c;
     while ( ((c = getchar()) != 255) ) {
 
@@ -128,23 +135,25 @@ int main () {
             c = getchar();
         }
 
-        unsigned char str[20] = {0};
-        int i = 0;
-        while ( (is_character(c)) && (c != 255)) {
-            lower_case(&c);
-            str[i] = c;
-            i++;
+        unsigned char str[50] = {0};
+        int tam = 0;
+        while ( (is_character(c)) && (c != 255) ) {
+            str[tam] = c;
+            tam++;
             c = getchar();
-        } 
-        
-        
-        if (dict_binary_search(&pt_dict, str) && c != 255) {
+        }
+
+        if (dict_binary_search(&pt_dict, str, tam) && c != 255) {
             printf("%s" , str);
         } else {
             printf("[%s]", str);
-        } 
+        }
         
+        if (c != 255) {
+            printf("%c", c);
+        }
     }
 
+    printf("\n");
     return 1;
 }
